db5c39185fde1034747a9bb96e02617aae1c31d0
bchagas99 <bchagas@ciandt.com>
2022-06-13 15:09:05 -0300

    alteração na classe PrintFormatacaoNumeros

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -4,7 +4,7 @@ public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf(new Locale("US"), "Valor: %,3.2f\n", valor);
+        System.out.printf(new Locale("US"), "Valor: R$ %,3.2f\n", valor);
         System.out.printf("Taxa: %.2f%%\n", taxa);
     }
 }
