3d0ef886320d4541f360a432213a4871ec0a7094
bchagas99 <bchagas@ciandt.com>
2022-06-13 12:18:38 -0300

    reduzindo arquivo PrintStringVariable para apenas 6 linhas

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,8 +1,6 @@
 public class PrintStringVariable {
    public static void main(String[] args) {
    String texto = "Holberton School";
-   System.out.println(texto);
-   System.out.println(texto);
-   System.out.println(texto.substring(0, 9));
+   System.out.printf("%1$s\n%1$s\n%2$s\n", texto, texto.substring(0, 9));
    }
  }
