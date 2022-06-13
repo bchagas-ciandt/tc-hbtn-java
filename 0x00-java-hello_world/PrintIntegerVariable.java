20b0a19b2f6d6f62869081b6ef5b5610c37b9049
bchagas99 <bchagas@ciandt.com>
2022-06-13 11:51:04 -0300

    alterando valor do inteiro de 1 pra 311

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,6 +1,6 @@
 public class PrintIntegerVariable {
     public static void main(String[] args) {
-    	int numero = 1;
+    	int numero = 311;
         System.out.printf("Avenida Andaló, %d\n", numero);
     }
 }
