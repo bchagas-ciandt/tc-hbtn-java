83cd3ce8a6bd6f4d627330af1568ff0c7a60c1b3
bchagas99 <bchagas@ciandt.com>
2022-06-14 11:36:12 -0300

    subindo diretorio /metodos e seus arquivos

---
+++ 0x04/metodos/PersonagemGame.java
@@ -0,0 +1,36 @@
+public class PersonagemGame {
+    private String nome;
+    private int saudeAtual;
+
+    public void tomarDano(int quantidadeDeDano) {
+        if (saudeAtual - quantidadeDeDano <= 0) {
+            saudeAtual = 0;
+        } else {
+          saudeAtual -= quantidadeDeDano;
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        if (saudeAtual + quantidadeDeCura >= 100) {
+            saudeAtual = 100;
+        } else {
+            saudeAtual += quantidadeDeCura;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+}
