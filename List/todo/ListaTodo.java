a74e3181ff23b0ac7c8740b4b8df4bafed63b99e
bchagas99 <bchagas@ciandt.com>
2022-06-27 17:44:20 -0300

    subindo subdiretorio List/todo

---
+++ List/todo/ListaTodo.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListaTodo {
+    private List<Tarefa> tarefas;
+
+    public ListaTodo() {
+        tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(Tarefa tarefa) {
+        for (Tarefa tarefa1 : tarefas) {
+            if (tarefa1.getIdentificador() == tarefa.getIdentificador()) {
+                throw new IllegalArgumentException("Tarefa com identificador "+ tarefa.getIdentificador() + " ja existente na lista");
+            }
+        }
+        tarefas.add(tarefa);
+    }
+
+    public boolean marcarTarefaFeita(int identificador) {
+        boolean isMarcada = false;
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getIdentificador() == identificador) {
+                tarefa.setEstahFeita(true);
+                isMarcada = true;
+                break;
+            }
+        }
+        return isMarcada;
+    }
+
+    public boolean desfazerTarefa(int identificador) {
+        boolean isMarcada = false;
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getIdentificador() == identificador) {
+                tarefa.setEstahFeita(false);
+                isMarcada = true;
+                break;
+            }
+        }
+        return isMarcada;
+    }
+
+    public void desfazerTodas() {
+        for (Tarefa tarefa : tarefas) {
+            tarefa.setEstahFeita(false);
+        }
+    }
+
+    public void fazerTodas() {
+        for (Tarefa tarefa : tarefas) {
+            tarefa.setEstahFeita(true);
+        }
+    }
+
+    public void listarTarefas() {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.isEstahFeita()) {
+                System.out.printf("[X]  Id: %d - Descricao: %s\n", tarefa.getIdentificador(), tarefa.getDescricao());
+            } else {
+                System.out.printf("[ ]  Id: %d - Descricao: %s\n", tarefa.getIdentificador(), tarefa.getDescricao());
+            }
+        }
+    }
+}
---
+++ List/todo/Tarefa.java
@@ -0,0 +1,40 @@
+public class Tarefa {
+    private String descricao;
+    private boolean estahFeita;
+    private int identificador;
+
+    public Tarefa(String descricao, int identificador) {
+        this.setDescricao(descricao);
+        this.identificador = identificador;
+    }
+
+    public void modificarDescricao(String descricaoNova) {
+        if (descricaoNova.isBlank() || descricaoNova == null) {
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+        this.descricao = descricaoNova;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        if (descricao.isBlank() || descricao == null) {
+            throw new IllegalArgumentException("Descricao de tarefa invalida");
+        }
+        this.descricao = descricao;
+    }
+
+    public boolean isEstahFeita() {
+        return estahFeita;
+    }
+
+    public void setEstahFeita(boolean estahFeita) {
+        this.estahFeita = estahFeita;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+}
