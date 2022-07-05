14d076aaa6400e373e5e165f3fb1712f89f5e6e2
bchagas99 <bchagas@ciandt.com>
2022-07-05 15:50:58 -0300

    subindo subdiretorio armazem_especifico

---
+++ Generics_Exercises/armazem_especifico/Armazem.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public abstract class Armazem<T> implements Armazenavel<T> {
+    private Map<String, T> map;
+
+    public Armazem() {
+        map = new TreeMap<>();
+    }
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        if (nome != null && valor != null) {
+            map.put(nome, valor);
+        }
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return map.get(nome);
+    }
+}
