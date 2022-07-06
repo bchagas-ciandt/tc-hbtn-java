13c6450b515a3c50e40be5fd722f18637634b83b
Bruno Chagas <107144507+bchagas99@users.noreply.github.com>
2022-07-06 12:55:25 -0300

    mudando Locale para US

--- Lambda/markup/Program.java
+++ Lambda/markup/Program.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("pt", "BR"));
+        Locale.setDefault(Locale.US);
         Produto produto1 = new Produto(129.99, "Mouse Gamer");
 
         System.out.printf("%s - %.2f - %.2f\n", produto1.getNome(),
