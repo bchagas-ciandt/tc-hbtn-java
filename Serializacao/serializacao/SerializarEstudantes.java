a1a6b92d28ee2aec4c0f3c903cb7a90ea42e14b5
bchagas99 <bchagas@ciandt.com>
2022-07-14 09:47:20 -0300

    alterando a classe SerializarEstudantes, refatorando

--- Serializacao/serializacao/SerializarEstudantes.java
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -1,9 +1,8 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SerializarEstudantes<T>{
-    private String nomeArquivo;
+    private final String nomeArquivo;
 
     public SerializarEstudantes(String nomeArquivo) {
         this.nomeArquivo = nomeArquivo;
@@ -16,26 +15,20 @@ public class SerializarEstudantes<T>{
             oos.writeObject(estudantes);
             oos.close();
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Nao foi possivel serializar");
         } catch (IOException e) {
             System.out.println("Nao foi possivel serializar");
         }
     }
 
     public List<T> desserializar() {
-        List<T> estudantes = new ArrayList<T>();
+        List<T> estudantes;
         try {
             FileInputStream fin = new FileInputStream(nomeArquivo);
             ObjectInputStream ois = new ObjectInputStream(fin);
             estudantes = (List<T>) ois.readObject();
             ois.close();
             return estudantes;
-        } catch (FileNotFoundException e) {
-            System.out.println("Nao foi possivel desserializar");
-        } catch (IOException e) {
-            System.out.println("Nao foi possivel desserializar");
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException | IOException e) {
             System.out.println("Nao foi possivel desserializar");
         }
         return null;
