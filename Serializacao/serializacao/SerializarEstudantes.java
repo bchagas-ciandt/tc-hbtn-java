9bd397fa081c6dc5d70538f9f87734e94eb7d37b
bchagas99 <bchagas@ciandt.com>
2022-07-14 09:34:47 -0300

    subindo subdiretorio Serializacao/serializacao

---
+++ Serializacao/serializacao/SerializarEstudantes.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.List;
+
+public class SerializarEstudantes<T>{
+    private String nomeArquivo;
+
+    public SerializarEstudantes(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void serializar(List<Estudante> estudantes) {
+        try {
+            FileOutputStream fout = new FileOutputStream(nomeArquivo);
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(estudantes);
+            oos.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Nao foi possivel serializar");
+        } catch (IOException e) {
+            System.out.println("Nao foi possivel serializar");
+        }
+    }
+
+    public List<Estudante> desserializar() {
+        List<Estudante> estudantes;
+        try {
+            FileInputStream fin = new FileInputStream(nomeArquivo);
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            estudantes = (List<Estudante>) ois.readObject();
+            ois.close();
+            return estudantes;
+        } catch (FileNotFoundException e) {
+            System.out.println("Nao foi possivel desserializar");
+        } catch (IOException e) {
+            System.out.println("Nao foi possivel desserializar");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Nao foi possivel desserializar");
+        }
+        return null;
+    }
+}
