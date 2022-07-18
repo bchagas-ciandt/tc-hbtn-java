10bda701bb7538852d6f51d5f2f420eeab87cee7
bchagas99 <bchagas@ciandt.com>
2022-07-18 12:14:37 -0300

    subindo classe NAME.java

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,25 @@
+import java.net.URL;
+import java.util.Date;
+
+public class Name {
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+    public List<Account> AccountList;
+
+    // Sem Prefixos
+    //A
+    public void deleteUser(Name.User pUser){};
+    //B
+    public void deleteUser(Name.User user){};
+
+    public URL url;
+
+    public class TableRepresent {};
+    public void saveCar(){};
+
+
+    public saveUser(){}; createAccount(){}; generateContract(){};
+
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+}
